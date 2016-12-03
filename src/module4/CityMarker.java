42fce6298bccea3d4ef10e7d9227442fa043df69
pawel franczak <pawel.fran@gmail.com>
2016-12-03 11:09:12 +0100

    initial commit

---
+++ src/module4/CityMarker.java
@@ -0,0 +1,73 @@
+package module4;
+
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import processing.core.PGraphics;
+
+/** Implements a visual marker for cities on an earthquake map
+ * 
+ * @author UC San Diego Intermediate Software Development MOOC team
+ * @author Your name here
+ *
+ */
+public class CityMarker extends SimplePointMarker {
+	
+	// The size of the triangle marker
+	// It's a good idea to use this variable in your draw method
+	public static final int TRI_SIZE = 5;  
+	
+	public CityMarker(Location location) {
+		super(location);
+	}
+	
+	
+	public CityMarker(Feature city) {
+		super(((PointFeature)city).getLocation(), city.getProperties());
+	}
+	
+	
+	// HINT: pg is the graphics object on which you call the graphics
+	// methods.  e.g. pg.fill(255, 0, 0) will set the color to red
+	// x and y are the center of the object to draw. 
+	// They will be used to calculate the coordinates to pass
+	// into any shape drawing methods.  
+	// e.g. pg.rect(x, y, 10, 10) will draw a 10x10 square
+	// whose upper left corner is at position x, y
+	/**
+	 * Implementation of method to draw marker on the map.
+	 */
+	public void draw(PGraphics pg, float x, float y) {
+		// Save previous drawing style
+		pg.pushStyle();
+		
+		// TODO: Add code to draw a triangle to represent the CityMarker
+		float h1 = (float)Math.sqrt(3) / 6;
+		float h2 = 2 * h1;
+		pg.fill(185, 94, 33);
+		float x1, x2, y1, y2, x3, y3;
+		x1 = x - 5f; y1 = y + 10 * h1; x2 = x + 5f; y2 = y + 10 * h1; x3 = x; y3 = y - 10 * h2;
+		pg.triangle(x1, y1, x2, y2, x3, y3);
+		// Restore previous drawing style
+		pg.popStyle();
+	}
+	
+	/* Local getters for some city properties.  You might not need these 
+	 * in module 4. 	 */
+	public String getCity()
+	{
+		return getStringProperty("name");
+	}
+	
+	public String getCountry()
+	{
+		return getStringProperty("country");
+	}
+	
+	public float getPopulation()
+	{
+		return Float.parseFloat(getStringProperty("population"));
+	}
+	
+}
